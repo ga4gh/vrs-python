7cd7b179bedca4eda73fd8da6739d096429dbd27
Reece Hart <reecehart@gmail.com>
2017-12-29 10:14:06 -0800

    added vmc/hgvs.py -- construct VMC Bundle from HGVS string

--- vmc/hgvs.py
+++ vmc/hgvs.py
@@ -1,11 +1,58 @@
-from . import models
+from . import models, computed_id
 from .seqrepo import get_vmc_sequence_id
 
 import hgvs
 import hgvs.parser
+import hgvs.location
 
-hp = hgvs.parser.Parser()
+hp = None
+def _get_hgvs_parser():
+    global hp
+    if hp is None:
+        hp = hgvs.parser.Parser()
+    return hp
 
 def from_hgvs(hgvs_string):
-    v = hp.parse_hgvs_variant
+    hp = _get_hgvs_parser()
+    sv = hp.parse_hgvs_variant(hgvs_string)
+
+    ir = models.Identifier(namespace="NCBI", accession=sv.ac)
+    sequence_id = get_vmc_sequence_id(ir)
+
+    if isinstance(sv.posedit.pos, hgvs.location.BaseOffsetInterval):
+        if sv.posedit.pos.start.is_intronic() or sv.posedit.pos.end.is_intronic():
+            raise ValueError("Intronic HGVS variants are not supported".format(sv.posedit.edit.type))
+            
+    if sv.posedit.edit.type == 'ins':
+        interval = models.Interval(start=sv.posedit.pos.start.base,
+                                   end=sv.posedit.pos.start.base)
+    elif sv.posedit.edit.type in ('sub', 'del', 'delins'):
+        interval = models.Interval(start=sv.posedit.pos.start.base - 1,
+                                   end=sv.posedit.pos.end.base)
+    else:
+        raise ValueError("HGVS variant type {} is unsupported".format(sv.posedit.edit.type))
     
+    location = models.Location(
+        sequence_id = sequence_id,
+        interval = interval)
+    location.id = computed_id(location)
+
+    allele = models.Allele(
+        location_id = location.id,
+        state = sv.posedit.edit.alt
+        )
+    allele.id = computed_id(allele)
+
+    bundle = models.Vmcbundle(
+        locations = {location.id: location.as_dict()},
+        alleles = {allele.id: allele.as_dict()},
+        identifiers = {sequence_id: [ir.as_dict()]},
+        )
+
+    import IPython; IPython.embed()	  ### TODO: Remove IPython.embed()
+
+    return bundle
+
+
+if __name__ == "__main__":
+    b = vmc.hgvs.from_hgvs("NC_000019.10:g.44908684C>T")
