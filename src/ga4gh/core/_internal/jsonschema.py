ede11cf062aed54ded29dd7c677f54f6c42cb2a8
Reece Hart <reecehart@gmail.com>
2019-08-15 18:49:53 -0700

    reorder modules functions

--- src/ga4gh/core/_internal/jsonschema.py
+++ src/ga4gh/core/_internal/jsonschema.py
@@ -8,7 +8,7 @@ import python_jsonschema_objects as pjs
 def build_models(path, standardize_names):
     """load models from json schema at path"""
     builder = pjs.ObjectBuilder(path)
-    models = builder.build_classes(standardize_names=False)  # TODO?: named_only=True
+    models = builder.build_classes(standardize_names=False)
     return models
 
 
@@ -17,16 +17,16 @@ def is_class(o):
     return isinstance(o, pjs.classbuilder.ProtocolBase)
 
 
-def is_literal(o):
-    """return True if object is a python jsonschema object literal"""
-    return isinstance(o, pjs.literals.LiteralValue)
-
-
 def is_identifiable(o):
     """return True if object is identifiable
 
     An object is considered identifiable if it contains an `_id` attribute
     """
     return is_class(o) and ("_id" in o)
+
+
+def is_literal(o):
+    """return True if object is a python jsonschema object literal"""
+    return isinstance(o, pjs.literals.LiteralValue)
 
 
