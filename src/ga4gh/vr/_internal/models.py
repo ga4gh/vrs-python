79e4b069e408f6c928ac01ecea57d844bd44dad7
Reece Hart <reecehart@gmail.com>
2019-11-26 21:19:14 -0800

    Enable optional VR_SCHEMA_DIR to refer to alternate schema directory, which must contain vr.json

--- src/ga4gh/vr/_internal/models.py
+++ src/ga4gh/vr/_internal/models.py
@@ -24,19 +24,30 @@ import pkg_resources
 from ga4gh.core import build_models
 
 
-schema_dir = pkg_resources.resource_filename(__name__, "data/schema")
+try:
+    schema_dir = os.environ["VR_SCHEMA_DIR"]
+except KeyError:
+    schema_dir = pkg_resources.resource_filename(__name__, "data/schema")
+
 schema_path = schema_dir + "/vr.json"
-schema_file = os.path.basename(schema_path)
+
+## Unused? Commenting out to see who complains. -Reece 2019-11-24
+# schema_file = os.path.basename(schema_path)
+
 
 models = None
-
 
 def _build_models():
     """load/reload models
 
+    This function facilitates reloading changes to the schema during
+    development.
+
     """
+
     global models
     models = build_models(schema_path, standardize_names=False)
     return models
+
 
 _build_models()
