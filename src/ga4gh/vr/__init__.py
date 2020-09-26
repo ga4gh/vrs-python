e0cc21ebf0daa3f001b011e153019277659a5e35
Reece Hart <reecehart@gmail.com>
2020-09-26 07:36:04 -0700

    add vrs symlink to quietly test whether this is a simple mechanism for migration

--- src/ga4gh/vr/__init__.py
+++ src/ga4gh/vr/__init__.py
@@ -8,9 +8,14 @@ implementation
 __all__ = """models normalize schema_path vr_deref vr_enref""".split()
 
 
-
-import warnings
 from pkg_resources import get_distribution, DistributionNotFound
+
+
+#if "vrs" not in __file__:
+#    import warnings
+#    raise FutureWarning("`ga4gh.vr` is being renamed to `ga4gh.vrs`. Migrate to `ga4gh.vrs`"
+#                            " now. The old name will be removed in the next release.")
+
 
 try:
     __version__ = get_distribution(__name__).version
@@ -22,5 +27,4 @@ finally:
 
 from ._internal.enderef import vr_deref, vr_enref
 from ._internal.models import models, schema_path
-
 from .normalize import normalize
