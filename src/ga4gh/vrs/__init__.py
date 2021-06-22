d806dd41ae236d4920d40a04468405ff90edaff7
Reece Hart <reecehart@gmail.com>
2021-06-21 22:02:54 -0700

    fix digesting of Haplotypes and VariationSets

--- src/ga4gh/vrs/__init__.py
+++ src/ga4gh/vrs/__init__.py
@@ -11,12 +11,6 @@ __all__ = """models normalize schema_path vrs_deref vrs_enref""".split()
 from pkg_resources import get_distribution, DistributionNotFound
 
 
-#if "vrs" not in __file__:
-#    import warnings
-#    raise FutureWarning("`ga4gh.vrs` is being renamed to `ga4gh.vrs`. Migrate to `ga4gh.vrs`"
-#                            " now. The old name will be removed in the next release.")
-
-
 try:
     __version__ = get_distribution(__name__).version
 except DistributionNotFound:    # pragma: nocover
@@ -28,8 +22,3 @@ finally:
 from ._internal.enderef import vrs_deref, vrs_enref
 from ._internal.models import models, schema_path
 from .normalize import normalize
-
-
-# backward compatibility
-vr_enref = vrs_enref
-vr_deref = vrs_deref
--- tests/validation/test_models.py
+++ tests/validation/test_models.py
@@ -1,32 +1,42 @@
+"""execute models validation tests from the VRS repo
+
+"""
+
 import os
 
 import pytest
 import yaml
 
-from ga4gh.core import ga4gh_serialize
+from ga4gh.core import ga4gh_serialize, ga4gh_digest, ga4gh_identify
 from ga4gh.vrs import models
 
+
+fxs = {
+    "ga4gh_serialize": lambda o: ga4gh_serialize(o).decode(),
+    "ga4gh_digest":  ga4gh_digest,
+    "ga4gh_identify": ga4gh_identify,
+    }
 
 validation_fn = os.path.join(os.path.dirname(__file__), "data", "models.yaml")
 validation_tests = yaml.load(open(validation_fn), Loader=yaml.SafeLoader)
 
+def flatten_tests(vts):
+    """flatten tests to (class, data, function name, exp out) tuples
 
-@pytest.mark.parametrize("test", validation_tests["SimpleInterval"])
-def test_SimpleInterval(test):
-    o = models.SimpleInterval(**test["in"])
-    assert test["out"]["ga4gh_serialize"] == ga4gh_serialize(o).decode()
+    Each tuple is a test in which an object of the specified class is
+    created with data, evaluated with the named function, and compared
+    with the expected output.
 
-#@pytest.mark.parametrize("test", validation_tests["NestedInterval"])
-#def test_NestedInterval(test):
-#    o = models.NestedInterval(**test["in"])
-#    assert test["out"]["ga4gh_serialize"] == ga4gh_serialize(o).decode()
+    """
+    for cls, tests in validation_tests.items():
+        for t in tests:
+            for fn, exp in t["out"].items():
+                yield pytest.param(cls, t["in"], fn, exp, id=f"{cls}-{fn}")
 
-@pytest.mark.parametrize("test", validation_tests["SequenceLocation"])
-def test_SequenceLocation(test):
-    o = models.SequenceLocation(**test["in"])
-    assert test["out"]["ga4gh_serialize"] == ga4gh_serialize(o).decode()
 
-@pytest.mark.parametrize("test", validation_tests["Allele"])
-def test_Allele(test):
-    o = models.Allele(**test["in"])
-    assert test["out"]["ga4gh_serialize"] == ga4gh_serialize(o).decode()
+@pytest.mark.parametrize("cls,data,fn,exp", flatten_tests(validation_tests))
+def test_validation(cls, data, fn, exp):
+    o = models[cls](**data)
+    fx = fxs[fn]
+    assert exp == fx(o)
+
