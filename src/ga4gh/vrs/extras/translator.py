32dc9673b6ea1d7ed425d034218788f2335dcaaf
James Stevenson <james.stevenson@nationwidechildrens.org>
2021-06-28 13:19:09 -0400

    Provide basic meta info to to_vcf

--- src/ga4gh/vrs/extras/translator.py
+++ src/ga4gh/vrs/extras/translator.py
@@ -21,7 +21,7 @@ import hgvs.dataproviders.uta
 
 from ga4gh.core import ga4gh_identify
 from ga4gh.vrs import models, normalize
-from ga4gh.vrs.extras.decorators import lazy_property  # this should be relocated
+from ga4gh.vrs.extras.decorators import lazy_property    # this should be relocated
 from ga4gh.vrs.utils.hgvs_tools import HgvsTools
 
 from pysam import VariantFile, VariantHeader, VariantRecord
@@ -46,7 +46,6 @@ class Translator:
     hgvs_re = re.compile(r"[^:]+:[cgnpr]\.")
     spdi_re = re.compile(r"(?P<ac>[^:]+):(?P<pos>\d+):(?P<del_len_or_seq>\w+):(?P<ins_seq>\w+)")
 
-
     def __init__(self,
                  data_proxy,
                  default_assembly_name="GRCh38",
@@ -59,7 +58,6 @@ class Translator:
         self.identify = identify
         self.normalize = normalize
         self.hgvs_tools = None
-
 
     def translate_from(self, var, fmt=None):
         """Translate variation `var` to VRS object
@@ -87,7 +85,6 @@ class Translator:
     def translate_to(self, vo, fmt):
         t = self.to_translators[fmt]
         return t(self, vo)
-
 
     ############################################################################
     ## INTERNAL
@@ -130,7 +127,6 @@ class Translator:
         allele = self._post_process_imported_allele(allele)
         return allele
 
-
     def _from_gnomad(self, gnomad_expr, assembly_name=None):
         """Parse gnomAD-style VCF expression into VRS Allele
 
@@ -169,7 +165,6 @@ class Translator:
         allele = self._post_process_imported_allele(allele)
         return allele
 
-
     def _from_hgvs(self, hgvs_expr):
         """parse hgvs into a VRS object (typically an Allele)
 
@@ -202,26 +197,19 @@ class Translator:
                 raise ValueError("Intronic HGVS variants are not supported ({sv.posedit})")
 
         if sv.posedit.edit.type == 'ins':
-            interval = models.SimpleInterval(start=sv.posedit.pos.start.base,
-                                        end=sv.posedit.pos.start.base)
+            interval = models.SimpleInterval(start=sv.posedit.pos.start.base, end=sv.posedit.pos.start.base)
             state = sv.posedit.edit.alt
         elif sv.posedit.edit.type in ('sub', 'del', 'delins', 'identity'):
-            interval = models.SimpleInterval(start=sv.posedit.pos.start.base - 1,
-                                       end=sv.posedit.pos.end.base)
+            interval = models.SimpleInterval(start=sv.posedit.pos.start.base - 1, end=sv.posedit.pos.end.base)
             if sv.posedit.edit.type == 'identity':
-                state = self.data_proxy.get_sequence(sv.ac,
-                                                     sv.posedit.pos.start.base - 1,
-                                                     sv.posedit.pos.end.base)
+                state = self.data_proxy.get_sequence(sv.ac, sv.posedit.pos.start.base - 1, sv.posedit.pos.end.base)
             else:
                 state = sv.posedit.edit.alt or ''
         elif sv.posedit.edit.type == 'dup':
 
-            interval = models.SimpleInterval(start=sv.posedit.pos.start.base - 1,
-                                             end=sv.posedit.pos.end.base)
+            interval = models.SimpleInterval(start=sv.posedit.pos.start.base - 1, end=sv.posedit.pos.end.base)
 
-            ref = self.data_proxy.get_sequence(sv.ac,
-                                               sv.posedit.pos.start.base - 1,
-                                               sv.posedit.pos.end.base)
+            ref = self.data_proxy.get_sequence(sv.ac, sv.posedit.pos.start.base - 1, sv.posedit.pos.end.base)
             state = ref + ref
         else:
             raise ValueError(f"HGVS variant type {sv.posedit.edit.type} is unsupported")
@@ -232,9 +220,7 @@ class Translator:
         allele = self._post_process_imported_allele(allele)
         return allele
 
-
     def _from_spdi(self, spdi_expr):
-
         """Parse SPDI expression in to a GA4GH Allele
 
         #>>> a = tlr.from_spdi("NM_012345.6:21:1:T")
@@ -272,7 +258,6 @@ class Translator:
         allele = models.Allele(location=location, state=sstate)
         allele = self._post_process_imported_allele(allele)
         return allele
-
 
     def _from_vrs(self, var):
         """convert from dict representation of VRS JSON to VRS object"""
@@ -353,12 +338,11 @@ class Translator:
          * worth trying to parse info fields to get an assembly ID?
          * enforce filter requirements?
         """
+        print('here')
         vcf_in = VariantFile(vcf_path)
         vrs_alleles = []
         for record in vcf_in:
-            vrs_alleles += self._from_vcf_record(record.chrom, record.pos,
-                                                 record.ref, record.alts,
-                                                 assembly_name)
+            vrs_alleles += self._from_vcf_record(record.chrom, record.pos, record.ref, record.alts, assembly_name)
 
         return vrs_alleles
 
@@ -385,7 +369,6 @@ class Translator:
         If the VRS object cannot be expressed as HGVS, raises ValueError.
 
         """
-
         def ir_stype(a):
             if a.startswith("refseq:NM_"):
                 return "n"
@@ -399,9 +382,8 @@ class Translator:
                 return "g"
             return None
 
-        if (type(vo).__name__ != "Allele"
-            or type(vo.location).__name__ != "SequenceLocation"
-            or type(vo.state).__name__ != "SequenceState"):
+        if (type(vo).__name__ != "Allele" or type(vo.location).__name__ != "SequenceLocation"
+                or type(vo.state).__name__ != "SequenceState"):
             raise ValueError(f"_to_hgvs requires a VRS Allele with SequenceLocation and SequenceState")
 
         sequence_id = str(vo.location.sequence_id)
@@ -427,20 +409,16 @@ class Translator:
             if start == end:    # insert: hgvs uses *exclusive coords*
                 ref = None
                 end += 1
-            else:               # else: hgvs uses *inclusive coords*
+            else:    # else: hgvs uses *inclusive coords*
                 ref = self.data_proxy.get_sequence(sequence_id, start, end)
                 start += 1
-            ival = hgvs.location.Interval(
-                start=hgvs.location.SimplePosition(base=start),
-                end=hgvs.location.SimplePosition(base=end))
-            alt = str(vo.state.sequence) or None  # "" => None
+            ival = hgvs.location.Interval(start=hgvs.location.SimplePosition(base=start),
+                                          end=hgvs.location.SimplePosition(base=end))
+            alt = str(vo.state.sequence) or None    # "" => None
             edit = hgvs.edit.NARefAlt(ref=ref, alt=alt)
 
         posedit = hgvs.posedit.PosEdit(pos=ival, edit=edit)
-        var = hgvs.sequencevariant.SequenceVariant(
-            ac=None,
-            type=stype,
-            posedit=posedit)
+        var = hgvs.sequencevariant.SequenceVariant(ac=None, type=stype, posedit=posedit)
 
         hgvs_exprs = []
         for alias in aliases:
@@ -482,9 +460,8 @@ class Translator:
 
         """
 
-        if (type(vo).__name__ != "Allele"
-            or type(vo.location).__name__ != "SequenceLocation"
-            or type(vo.state).__name__ != "SequenceState"):
+        if (type(vo).__name__ != "Allele" or type(vo.location).__name__ != "SequenceLocation"
+                or type(vo.state).__name__ != "SequenceState"):
             raise ValueError(f"_to_hgvs requires a VRS Allele with SequenceLocation and SequenceState")
 
         sequence_id = str(vo.location.sequence_id)
@@ -497,16 +474,46 @@ class Translator:
         spdis = [a + spdi_tail for a in aliases]
         return spdis
 
-    def _allele_to_vcf(self, vcfh, vo, namespace="refseq"):
+    vcf_info_params = {
+        'AA': (1, 'String', 'Ancestral allele'),
+        'AC': ('A', 'Integer', 'Allele count in genotypes, for each ALT allele, in the same order as listed'),
+        'AD': ('R', 'Integer', 'Total read depth for each allele'),
+        'ADF': ('R', 'Integer', 'Read depth for each allele on the forward strand'),
+        'ADR': ('R', 'Integer', 'Read depth for each allele on the reverse strand'),
+        'AF':
+        ('A', 'Float',
+         'Allele frequency for each ALT allele in the same order as listed (estimated from primary data not called genotypes)'
+         ),
+        'AN': (1, 'Integer', 'Total number of alleles in called genotypes'),
+        'BQ': (1, 'Float', 'RMS base quality'),
+        'CIGAR': ('A', 'String', 'Cigar string describing how to align an alternate allele to the reference allele'),
+        'DB': (0, 'Flag', 'dbSNP membership'),
+        'DP': (1, 'Integer', 'Combined depth across samples'),
+        'END': (1, 'Integer', 'End position on CHROM'),
+        'H2': (0, 'Flag', 'HapMap2 membership'),
+        'H3': (0, 'Flag', 'HapMap3 membership'),
+        'MQ': (1, 'Float', 'RMS mapping quality'),
+        'MQ0': (1, 'Integer', 'Number of MAPQ == 0 reads'),
+        'NS': (1, 'Integer', 'Number of samples with data'),
+        'SB': (4, 'Integer', 'Strand bias'),
+        'SOMATIC': (0, 'Flag', 'Somatic mutation'),
+        'VALIDATED': (0, 'Flag', 'Validated by follow-up experiment'),
+        '1000G': (0, 'Flag', '1000 Genomes membership')
+    }
+
+    def _allele_to_vcf(self, vcfh, vo, namespace="refseq", info={}):
         """Given a Pysam VariantHeader object `vcfh`, and a VRS Allele object
-        `vo`, return a Pysam VariantRecord
+        `vo`, return a Pysam VariantRecord. Optionally provide a `namespace`
+        string for sequence ID translation purposes, and/or an `info` dict
+        keying valid reserved INFO keys to values for the record.
+
+        Raises ValueError if given unrecognized INFO key.
 
         TODO
          * more elegant way of extracting chrom
         """
-        if (type(vo).__name__ != "Allele"
-            or type(vo.location).__name__ != "SequenceLocation"
-            or type(vo.state).__name__ != "SequenceState"):
+        if (type(vo).__name__ != "Allele" or type(vo.location).__name__ != "SequenceLocation"
+                or type(vo.state).__name__ != "SequenceState"):
             raise ValueError(f"_to_vcf requires a VRS Allele with SequenceLocation and SequenceState")
 
         start = int(vo.location.interval.start)
@@ -516,8 +523,7 @@ class Translator:
         interval_length = end - start
         sequence_id = str(vo.location.sequence_id)
         ref_sequence = self.data_proxy.get_sequence(sequence_id, start, end)
-        aliases = self.data_proxy.translate_sequence_identifier(sequence_id,
-                                                                namespace)
+        aliases = self.data_proxy.translate_sequence_identifier(sequence_id, namespace)
 
         if interval_length == alt_sequence_length:
             # SNVs/MNVs
@@ -531,47 +537,67 @@ class Translator:
             pos = start - 1
         else:
             # ins
-            diff_bases = [i[2] for i in ndiff(ref_sequence[::-1], alt[::-1])
-                          if i.startswith('+ ')][::-1]
+            diff_bases = [i[2] for i in ndiff(ref_sequence[::-1], alt[::-1]) if i.startswith('+ ')][::-1]
             diff = ''.join(diff_bases)
             pos = start - 1
             ref = self.data_proxy.get_sequence(sequence_id, start - 1, start)
             alt = ref + diff
 
         if namespace == 'refseq':
-            chrom = str(int(aliases[0][14:16]))  # drop leading 0
+            chrom = str(int(aliases[0][14:16]))    # drop leading 0
         else:
-            raise NotImplementedError  # TODO
+            raise NotImplementedError    # TODO
         if chrom not in vcfh.contigs.keys():
             vcfh.add_meta('contig', items=[('ID', chrom)])
 
-        record = vcfh.new_record(contig=chrom, start=pos,
-                                 alleles=(ref, alt))
+        for key in info.keys():
+            if key not in vcfh.info.keys():
+                key_meta = self.vcf_info_params.get(key)
+                if not key_meta:
+                    raise ValueError(f'Unrecognized INFO key: {key}')
+                vcfh.info.add(key, *key_meta)
+
+        record = vcfh.new_record(contig=chrom, start=pos, alleles=(ref, alt), info=info)
         return record
 
-    def _to_vcf(self, vrs_objects, file_path, namespace="refseq"):
+    def _to_vcf(self, vrs_objects, file_path, namespace="refseq", info=[]):
         """Given an iterable collection of VRS Alleles, write to `file_path`
-        a basic VCF file.
+        a basic VCF file. Optionally provide a List of Dicts, which must be
+        of length equal to `vrs_objects`, where `info[i]` contains keys and
+        values for valid INFO fields for `vrs_objects[i]`. Keys and values
+        must adhere to the reserved INFO keys as defined in the VCF 4.3 spec.
 
-        Raises ValueError if provided list contains non-Allele objects, or
-        if they don't have SequenceLocation and SequenceState attributes.
+        Raises ValueError if
+         * Provided list contains non-Allele objects or if they don't have
+           SequenceLocation and SequenceState attributes.
+         * if len(info) != 0 and len(info) != len(vrs_objects)
 
         WORKING
          * basic SNVs, insertions, deletions
+         * collapsing alleles at same position into single records
 
         TODO
          * be more intelligent about getting namespace IDs
          * other variation types
         """
+        if len(info) != 0 and len(info) != len(vrs_objects):
+            raise ValueError(f'length of info param must be either 0 or '
+                             f'== len(vrs_objects) == {len(vrs_objects)}, '
+                             f'got {len(info)} instead.')
+
         vcfh = VariantHeader()
         records = []
 
-        for vo in vrs_objects:
-            records.append(self._allele_to_vcf(vcfh, vo, namespace))
+        if info:
+            for vo, vo_info in zip(vrs_objects, info):
+                records.append(self._allele_to_vcf(vcfh, vo, namespace, vo_info))
+        else:
+            for vo in vrs_objects:
+                records.append(self._allele_to_vcf(vcfh, vo, namespace))
 
         records_grouped = {}
         for record in records:
-            key = (record.chrom, record.pos)
+            key = (record.chrom, record.pos, record.ref)
             if key in records_grouped.keys():
                 records_grouped[key] += [record]
             else:
@@ -583,9 +609,7 @@ class Translator:
                 records_out.append(group[0])
             else:
                 alts = ','.join([r.alts[0] for r in group])
-                record = vcfh.new_record(contig=group[0].chrom,
-                                         start=group[0].pos - 1,
-                                         alleles=(group[0].ref, alts))
+                record = vcfh.new_record(contig=group[0].chrom, start=group[0].pos - 1, alleles=(group[0].ref, alts))
                 records_out.append(record)
 
         records_out.sort(key=lambda r: ((r.chrom).zfill(2), r.pos))
@@ -595,13 +619,11 @@ class Translator:
             vcf.write(record)
         vcf.close()
 
-
     @lazy_property
     def _hgvs_parser(self):
         """instantiates and returns an hgvs parser instance"""
         _logger.info("Creating  parser")
         return hgvs.parser.Parser()
-
 
     def _post_process_imported_allele(self, allele):
         """Provide common post-processing for imported Alleles IN-PLACE.
@@ -620,12 +642,10 @@ class Translator:
 
         return allele
 
-
     def _seq_id_mapper(self, ir):
         if self.translate_sequence_identifiers:
             return self.data_proxy.translate_sequence_identifier(ir, "ga4gh")[0]
         return ir
-
 
     from_translators = {
         "beacon": _from_beacon,
@@ -639,7 +659,7 @@ class Translator:
     to_translators = {
         "hgvs": _to_hgvs,
         "spdi": _to_spdi,
-        #"gnomad": to_gnomad,
+    #"gnomad": to_gnomad,
         "vcf": _to_vcf,
     }
 
@@ -653,11 +673,7 @@ if __name__ == "__main__":
     tlr = Translator(data_proxy=dp)
 
     expressions = [
-        "bogus",
-        "1-55516888-G-GA",
-        "13 : 32936732 G > C",
-        "NC_000013.11:g.32936732G>C",
-        "NM_000551.3:21:1:T", {
+        "bogus", "1-55516888-G-GA", "13 : 32936732 G > C", "NC_000013.11:g.32936732G>C", "NM_000551.3:21:1:T", {
             'location': {
                 'interval': {
                     'end': 22,
