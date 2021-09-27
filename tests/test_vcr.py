d15f6ba832f5a8f300e23cb05eacf9c813d3d40c
korikuzma <korikuzma@gmail.com>
2021-09-27 09:31:36 -0400

    vcr and normalize tests use rest_dataproxy fixture

--- tests/test_vcr.py
+++ tests/test_vcr.py
@@ -2,7 +2,7 @@ import pytest
 
 
 @pytest.mark.vcr
-def test_vcrtest(dataproxy):
-    seq = dataproxy.get_sequence("NC_000013.11", 50_000_000, 50_000_050)
+def test_vcrtest(rest_dataproxy):
+    seq = rest_dataproxy.get_sequence("NC_000013.11", 50_000_000, 50_000_050)
     assert len(seq) == 50
     assert seq == "TTAGGTGTTTAGATGATTTCTAAGATGCTTTTAAGCCCAGTATTTCTATT"
