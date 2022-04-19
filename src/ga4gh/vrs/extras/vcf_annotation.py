9e80a11ec3e9c5c6724a9cfd1eb20901ff334087
wesleygoar <wesley.goar@nationwidechildrens.org>
2022-04-19 10:57:46 -0500

    Resolved_comments

--- src/ga4gh/vrs/extras/vcf_annotation.py
+++ src/ga4gh/vrs/extras/vcf_annotation.py
@@ -7,23 +7,16 @@ Output Format: VCF
 """
 
 import argparse
-import hashlib
-import logging
 import sys
 import pickle
 import time
-import coloredlogs
 from biocommons.seqrepo import SeqRepo
-from pysam import VariantFile
-from ga4gh.core import ga4gh_identify
-from ga4gh.vrs import models
+import pysam
 from ga4gh.vrs.dataproxy import SeqRepoDataProxy
 from ga4gh.vrs.extras.translator import Translator
 
 dp = SeqRepoDataProxy(SeqRepo("/usr/local/share/seqrepo/latest"))
 tlr = Translator(data_proxy=dp)
-
-_logger = logging.getLogger()
 
 INFO_FIELD_ID = "VRS_Allele"
 vrs_data = {}
@@ -33,17 +26,12 @@ def parse_args(argv):
     ap = argparse.ArgumentParser()
     ap.add_argument("VCF_IN")
     ap.add_argument("--out", "-o", default="-")
-    ap.add_argument("--failure-probability", "-f", type=float, default=0)
-    ap.add_argument("--vrs_file", default="-")
+    ap.add_argument("--vrs-file", default="-")
     opts = ap.parse_args(argv)
     return opts
 
 
 def record_digests(record):
-
-    def digest(d):
-        return hashlib.sha512(d.encode("ascii")).hexdigest()[:8]
-
     gnomad_loc = f"{record.chrom}-{record.pos}"
     alts = record.alts if record.alts else []
     data = f"{record.chrom}\t{record.pos}\t{record.id}\t{record.ref}\t{record.alts}"
@@ -67,14 +55,13 @@ def record_digests(record):
 
 if __name__ == "__main__":
     start_time = time.time()
-    coloredlogs.install(level="INFO")
 
     opts = parse_args(sys.argv[1:])
     print(f"These are the options that you have selected: {opts}")
 
-    vcf_in = VariantFile(filename=opts.VCF_IN)
+    vcf_in = pysam.VariantFile(filename=opts.VCF_IN)
     vcf_in.header.info.add(INFO_FIELD_ID, "1", "String", "vrs")
-    vcf_out = VariantFile(opts.out, "w", header=vcf_in.header)
+    vcf_out = pysam.VariantFile(opts.out, "w", header=vcf_in.header)
     vrs_out = open(opts.vrs_file, "wb")    #For sending VRS data to the pickle file
 
     for record in vcf_in:
