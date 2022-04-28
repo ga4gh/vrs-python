097799eab117a3577b58dbb27a93c8101b5f1c57
korikuzma <korikuzma@gmail.com>
2022-04-28 13:27:59 -0400

    Rename vcf test files, clean up test_annotate_vcf, + add test data to seqrepo

--- tests/extras/test_vcf_annotation.py
+++ tests/extras/test_vcf_annotation.py
@@ -1,17 +1,6 @@
-"""
-Purpose: Ensure proper functionality of VCF_Annotation.py
-
-Input Format: VCF
-Output Format: VCF
-
-Function 1: Test proper vrs_allele_id creation
-    - 
-Function 2: Test addition of data to VCF file
-
-"""
+"""Ensure proper functionality of VCFAnnotator"""
 import gzip
 import os
-import io
 
 import pytest
 
@@ -31,18 +20,20 @@ def vcf_annotator(tlr_local):
 
 @pytest.mark.vcr
 def test_annotate_vcf(vcf_annotator):
-    input_vcf = "tests/extras/data/building_new_vcf.vcf"
-    output_vcf = "tests/extras/data/test_vcf_out.vcf.gz"
-    output_vrs_pkl = "tests/extras/data/test_vcf_pkl.pkl"
-    expected_vcf = "tests/extras/data/expected_output.vcf.gz"
+    TEST_DATA_DIR = "tests/extras/data"
+
+    input_vcf = f"{TEST_DATA_DIR}/test_vcf_input.vcf"
+    output_vcf = f"{TEST_DATA_DIR}/test_vcf_output.vcf.gz"
+    output_vrs_pkl = f"{TEST_DATA_DIR}/test_vcf_pkl.pkl"
+    expected_vcf = f"{TEST_DATA_DIR}/test_vcf_expected_output.vcf.gz"
     vcf_annotator.annotate(input_vcf, output_vcf, output_vrs_pkl)
 
     with gzip.open(output_vcf, "rt") as out_vcf:
         out_vcf_lines = out_vcf.readlines()
     with gzip.open(expected_vcf, "rt") as expected_output:
         expected_output_lines = expected_output.readlines()
-        assert out_vcf_lines == expected_output_lines
 
+    assert out_vcf_lines == expected_output_lines
     assert os.path.exists(output_vrs_pkl)
 
     os.remove(output_vcf)
