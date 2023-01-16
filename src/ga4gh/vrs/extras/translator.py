7cd0c830a7bd2fa624063cce8e3fd90fb9cf89cb
korikuzma <korikuzma@gmail.com>
2023-01-16 11:34:50 -0500

    add require_validation param to translate_from
    - Raise ValidationError if set to True and validation checks fail

--- src/ga4gh/vrs/extras/translator.py
+++ src/ga4gh/vrs/extras/translator.py
@@ -4,7 +4,7 @@ Input formats: VRS (serialized), hgvs, spdi, gnomad (vcf), beacon
 Output formats: VRS (serialized), hgvs, spdi, gnomad (vcf)
 
 """
-
+from typing import Tuple
 from collections.abc import Mapping
 import logging
 import re
@@ -23,6 +23,12 @@ from ga4gh.vrs.extras.decorators import lazy_property  # this should be relocate
 from ga4gh.vrs.utils.hgvs_tools import HgvsTools
 
 _logger = logging.getLogger(__name__)
+
+
+class ValidationError(Exception):
+    """Class for validation errors during translation"""
+
+    pass
 
 
 class Translator:
@@ -56,24 +62,28 @@ class Translator:
         self.hgvs_tools = None
 
 
-    def translate_from(self, var, fmt=None):
+    def translate_from(self, var, fmt=None, require_validation=True):
         """Translate variation `var` to VRS object
 
         If `fmt` is None, guess the appropriate format and return the variant.
         If `fmt` is specified, try only that format.
+        If `require_validation` is `True` then validation checks must pass in order to
+            return a VRS object. A `ValidationError` will be raised if validation checks
+            fail. If `False` then VRS object will be returned even if validation checks
+            fail.
 
         See also notes about `from_` and `to_` methods.
         """
 
         if fmt:
             t = self.from_translators[fmt]
-            o = t(self, var)
+            o = t(self, var, require_validation=require_validation)
             if o is None:
                 raise ValueError(f"Unable to parse data as {fmt} variation")
             return o
 
         for fmt, t in self.from_translators.items():
-            o = t(self, var)
+            o = t(self, var, require_validation=require_validation)
             if o:
                 return o
 
@@ -106,7 +116,7 @@ class Translator:
     ############################################################################
     ## INTERNAL
 
-    def _from_beacon(self, beacon_expr, assembly_name=None):
+    def _from_beacon(self, beacon_expr, assembly_name=None, require_validation=True):
         """Parse beacon expression into VRS Allele
 
         #>>> a = tlr.from_beacon("19 : 44908822 C > T")
@@ -184,8 +194,10 @@ class Translator:
         end = start + len(ref)
         ins_seq = alt
 
-        if require_validation and not self._is_valid_ref_seq(sequence_id, start, end, ref):
-            return None
+        # validation checks
+        valid_ref_seq, err_msg = self._is_valid_ref_seq(sequence_id, start, end, ref)
+        if not valid_ref_seq and require_validation:
+            raise ValidationError(err_msg)
 
         interval = models.SequenceInterval(start=models.Number(value=start),
                                            end=models.Number(value=end))
@@ -196,7 +208,7 @@ class Translator:
         return allele
 
 
-    def _from_hgvs(self, hgvs_expr):
+    def _from_hgvs(self, hgvs_expr, require_validation=True):
         """parse hgvs into a VRS object (typically an Allele)
 
         #>>> a = tlr.from_hgvs("NM_012345.6:c.22A>T")
@@ -265,7 +277,7 @@ class Translator:
         return allele
 
 
-    def _from_spdi(self, spdi_expr):
+    def _from_spdi(self, spdi_expr, require_validation=True):
 
         """Parse SPDI expression in to a GA4GH Allele
 
@@ -310,7 +322,7 @@ class Translator:
         return allele
 
 
-    def _from_vrs(self, var):
+    def _from_vrs(self, var, require_validation=True):
         """convert from dict representation of VRS JSON to VRS object"""
         if not isinstance(var, Mapping):
             return None
@@ -458,7 +470,8 @@ class Translator:
         return spdis
 
 
-    def _is_valid_ref_seq(self, sequence_id: str, start_pos: int, end_pos: int, ref: str) -> bool:
+    def _is_valid_ref_seq(self, sequence_id: str, start_pos: int, end_pos: int,
+                          ref: str) -> Tuple[bool, str]:
         """Return wether or not the expected reference sequence matches the actual reference sequence
 
         :param str sequence_id: Sequence ID to use
@@ -466,15 +479,17 @@ class Translator:
         :param int end_pos: End pos (inter-residue) on the sequence_id
         :param str ref: The expected reference sequence on the sequence_id given the
             start_pos and end_pos
-        :return: `True` if the actual reference sequence matches the expected reference
-            sequence. `False` otherwise.
+        :return: Tuple containing whether or not actual reference sequence matches
+            the expected reference sequence and error message if mismatch
         """
         actual_ref = self.data_proxy.get_sequence(sequence_id, start_pos, end_pos)
         is_valid = actual_ref == ref
+        err_msg = ""
         if not is_valid:
-            _logger.warning("Expected reference sequence %s on %s at positions (%i, %i) "
-                            "but found %s", ref, sequence_id, start_pos, end_pos, actual_ref)
-        return is_valid
+            err_msg = f"Expected reference sequence {ref} on {sequence_id} at positions "\
+                      f"({start_pos}, {end_pos}) but found {actual_ref}"
+            _logger.warning(err_msg)
+        return is_valid, err_msg
 
 
     def is_valid_allele(self, vo):
