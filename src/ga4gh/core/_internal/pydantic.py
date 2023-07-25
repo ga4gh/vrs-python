5357c9c4dc3f2c1e6260af43ddc0e4fd9ee380e7
Kyle Ferriter <kferrite@broadinstitute.org>
2023-07-25 13:18:55 -0400

    Implement is_curie_type

--- src/ga4gh/core/_internal/pydantic.py
+++ src/ga4gh/core/_internal/pydantic.py
@@ -1,9 +1,9 @@
-from pydantic import BaseModel
 import re
-from ga4gh.vrs.models import IRI
+from typing import Any
+from pydantic import BaseModel
 
 
-def is_identifiable(o: any) -> bool:
+def is_identifiable(o: Any) -> bool:
     """
     Determine if object is identifiable. An object is considered identifiable if
     contains a `ga4gh_digest` attribute
@@ -14,21 +14,28 @@ def is_identifiable(o: any) -> bool:
     return hasattr(o, "ga4gh_digest")
 
 
-def is_literal(o: any) -> bool:
+def is_literal(o: Any) -> bool:
     return isinstance(o, (str, int, float, complex, bool))
 
 
-def is_list(o: any) -> bool:
+def is_list(o: Any) -> bool:
     return isinstance(o, list)
 
 
-def is_curie_type(o: any) -> bool:
-    # if isinstance(o, pydantic.BaseModel):
-    # return isinstance(o, IRI) or re.match(r'[a-zA-Z0-9.]+:\S+')
-    raise RuntimeError("Not implemented: is_curie_type")
+def is_curie_type(o: Any) -> bool:
+    """
+    Returns true if the object is a str-like matching the CURIE pattern.
+    If object is a Pydantic custom root type, extracts the value first,
+    which enables (for example) a GA4GH IRI pydantic model object to be passed.
+    """
+    if isinstance(o, BaseModel) and hasattr(o, "__root__"):
+        o = o.__root__
+    return re.match(r'[a-zA-Z0-9.]+:\S+', o)
 
 
-def is_pydantic_instance(o: any) -> bool:
+
+
+def is_pydantic_instance(o: Any) -> bool:
     return isinstance(o, BaseModel)
 
 
