ebd120d85afa51795c87db513c2bf308c64ecd1e
Kori Kuzma <korikuzma@gmail.com>
2023-08-31 18:56:49 -0400

    refactor: replace pkg_resources with importlib.metadata (#236)
    * replace pkg_resources with importlib.metadata
    * rm reference to vrsatile schema dir

--- src/ga4gh/vrs/__init__.py
+++ src/ga4gh/vrs/__init__.py
@@ -1,21 +1,18 @@
 # flake8: noqa
 """Public interface to the GA4GH Variation Representation reference
 implementation
-
 """
-
-from pkg_resources import get_distribution, DistributionNotFound
+from importlib.metadata import version, PackageNotFoundError
 
 from .normalize import normalize
 from ._internal.enderef import vrs_deref, vrs_enref
 from ._internal import models
-schema_path = models.schema_path
 
 __all__ = """models normalize schema_path vrs_deref vrs_enref""".split()
 
 try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:    # pragma: nocover
+    __version__ = version(__name__)
+except PackageNotFoundError:    # pragma: nocover
     __version__ = "unknown"
 finally:
-    del get_distribution, DistributionNotFound
+    del version, PackageNotFoundError
