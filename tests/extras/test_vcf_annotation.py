90d6b1f14692fe566748cd68a25ad58ed4fe930d
Eugene Clark <ehclark@partners.org>
2023-12-14 09:51:44 -0500

    Added unit test for VCF annotation with compute_for_ref=False

--- tests/extras/test_vcf_annotation.py
+++ tests/extras/test_vcf_annotation.py
@@ -19,6 +19,7 @@ def test_annotate_vcf(vcf_annotator):
     output_vcf = f"{TEST_DATA_DIR}/test_vcf_output.vcf.gz"
     output_vrs_pkl = f"{TEST_DATA_DIR}/test_vcf_pkl.pkl"
     expected_vcf = f"{TEST_DATA_DIR}/test_vcf_expected_output.vcf.gz"
+    expected_altsonly_vcf = f"{TEST_DATA_DIR}/test_vcf_expected_altsonly_output.vcf.gz"
     expected_vcf_no_vrs_attrs = f"{TEST_DATA_DIR}/test_vcf_expected_output_no_vrs_attrs.vcf.gz"
 
     # Test GRCh38 assembly, which was used for input_vcf and no vrs attributes
@@ -43,10 +44,23 @@ def test_annotate_vcf(vcf_annotator):
     os.remove(output_vcf)
     os.remove(output_vrs_pkl)
 
+    # Test GRCh38 assembly with VRS computed for ALTs only, which was used for input_vcf and vrs attributes
+    vcf_annotator.annotate(input_vcf, output_vcf, output_vrs_pkl, vrs_attributes=True, compute_for_ref=False)
+    with gzip.open(output_vcf, "rt") as out_vcf:
+        out_vcf_lines = out_vcf.readlines()
+    with gzip.open(expected_altsonly_vcf, "rt") as expected_output:
+        expected_output_lines = expected_output.readlines()
+    assert out_vcf_lines == expected_output_lines
+    assert os.path.exists(output_vrs_pkl)
+    os.remove(output_vcf)
+    os.remove(output_vrs_pkl)
+
     # Test GRCh37 assembly, which was not used for input_vcf
     vcf_annotator.annotate(input_vcf, output_vcf, output_vrs_pkl, vrs_attributes=True, assembly="GRCh37")
     with gzip.open(output_vcf, "rt") as out_vcf:
         out_vcf_lines = out_vcf.readlines()
+    with gzip.open(expected_vcf, "rt") as expected_output:
+        expected_output_lines = expected_output.readlines()
     assert out_vcf_lines != expected_output_lines
     assert os.path.exists(output_vrs_pkl)
     os.remove(output_vcf)
