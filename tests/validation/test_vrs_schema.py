489b6279d305de4e3cc1f22755eac30ab4df474a
Kori Kuzma <korikuzma@gmail.com>
2024-03-26 13:15:20 -0400

    chore: remove submodules from package data (#371)
    * Since submodules are no longer used to generate python classes (we manually create Pydantic classes), this removes them from package data but retains for tests. This also updates the vrs submodule

--- tests/validation/test_vrs_schema.py
+++ tests/validation/test_vrs_schema.py
@@ -1,24 +1,28 @@
 """test that VRS Python model structures match VRS Schema
 """
-import yaml
+import json
 from pathlib import Path
+
 from ga4gh.vrs import models
 
 ROOT_DIR = Path(__file__).parents[2]
-VRS_SCHEMA_DIR = ROOT_DIR / 'submodules' / 'vrs' / 'schema'
-
-with open(VRS_SCHEMA_DIR / 'vrs.yaml') as vrs_yaml:
-    VRS_SCHEMA = yaml.safe_load(vrs_yaml)
+VRS_SCHEMA_DIR = ROOT_DIR / 'submodules' / 'vrs' / 'schema' / 'vrs' / 'json'
+VRS_SCHEMA = {}
 
 VRS_CONCRETE_CLASSES = set()
 VRS_PRIMITIVES = set()
 
-for vrs_class in VRS_SCHEMA['$defs']:
-    cls_def = VRS_SCHEMA['$defs'][vrs_class]
-    if 'properties' in cls_def:
+for f in VRS_SCHEMA_DIR.glob("*"):
+    with open(f, "r") as rf:
+        cls_def = json.load(rf)
+
+    vrs_class = cls_def["title"]
+    VRS_SCHEMA[vrs_class] = cls_def
+    if "properties" in cls_def:
         VRS_CONCRETE_CLASSES.add(vrs_class)
-    elif cls_def.get('type') in ['array', 'int', 'str']:
+    elif cls_def.get("type") in {"array", "int", "str"}:
         VRS_PRIMITIVES.add(vrs_class)
+
 
 NOT_IMPLEMENTED = ['Adjacency', 'Haplotype']  # Use this to skip testing of not-implemented classes
                                               # TODO: Remove this once 2.0 models at beta
@@ -39,7 +43,7 @@ def test_schema_class_fields_are_valid():
     for vrs_class in VRS_CONCRETE_CLASSES:
         if vrs_class in NOT_IMPLEMENTED:
             continue
-        schema_fields = set(VRS_SCHEMA['$defs'][vrs_class]['properties'])
+        schema_fields = set(VRS_SCHEMA[vrs_class]['properties'])
         pydantic_model = getattr(models, vrs_class)
         assert set(pydantic_model.__fields__) == schema_fields, vrs_class
 
@@ -50,12 +54,12 @@ def test_model_keys_are_valid():
     for vrs_class in VRS_CONCRETE_CLASSES:
         if vrs_class in NOT_IMPLEMENTED:
             continue
-        if VRS_SCHEMA['$defs'][vrs_class].get('ga4ghDigest', {}).get('keys', None) is None:
+        if VRS_SCHEMA[vrs_class].get('ga4ghDigest', {}).get('keys', None) is None:
             continue
         pydantic_model = getattr(models, vrs_class)
         try:
             pydantic_model_digest_keys = pydantic_model.ga4gh.keys
         except AttributeError:
             raise AttributeError(vrs_class)
-        assert set(pydantic_model_digest_keys) == set(VRS_SCHEMA['$defs'][vrs_class]['ga4ghDigest']['keys']), vrs_class
+        assert set(pydantic_model_digest_keys) == set(VRS_SCHEMA[vrs_class]['ga4ghDigest']['keys']), vrs_class
         assert pydantic_model_digest_keys == sorted(pydantic_model.ga4gh.keys), vrs_class
