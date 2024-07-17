740e4e6c8dd11872371d53f13ced8d4105236c8d
Kori Kuzma <korikuzma@gmail.com>
2024-07-17 09:17:57 -0400

    rm parse_ga4gh_identifier

--- src/ga4gh/core/identifiers.py
+++ src/ga4gh/core/identifiers.py
@@ -24,7 +24,7 @@ from pydantic import BaseModel
 
 from ga4gh.core.pydantic import get_pydantic_root
 
-__all__ = "ga4gh_digest ga4gh_identify ga4gh_serialize is_ga4gh_identifier parse_ga4gh_identifier".split()
+__all__ = "ga4gh_digest ga4gh_identify ga4gh_serialize is_ga4gh_identifier".split()
 
 CURIE_NAMESPACE = "ga4gh"
 CURIE_SEP = ":"
