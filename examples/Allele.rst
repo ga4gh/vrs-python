e77c24685015d46179781e816cfe348da5d8c93f
Kori Kuzma <korikuzma@gmail.com>
2024-10-14 19:01:36 -0400

    wip: initial work for pydantic -> rst

---
+++ examples/Allele.rst
@@ -0,0 +1,50 @@
+.. list-table::
+   :class: clean-wrap
+   :header-rows: 1
+   :align: left
+   :widths: auto
+
+   * - Field
+     - Type
+     - Limits
+     - Description
+   * - id
+     - string
+     - 0..1
+     - The 'logical' identifier of the Entity in the system of record, e.g. a UUID.  This 'id' is unique within a given system, but may or may not be globally unique outside the system. It is used within a system to reference an object from another.
+   * - type
+     - None
+     - 1..1
+     - MUST be "Allele"
+   * - label
+     - string
+     - 0..1
+     - A primary name for the entity.
+   * - description
+     - string
+     - 0..1
+     - A free-text description of the Entity.
+   * - alternativeLabels
+     - string
+     - 0..m
+     - Alternative name(s) for the Entity.
+   * - extensions
+     - :ref:`Extension`
+     - 0..m
+     - A list of extensions to the Entity, that allow for capture of information not directly supported by elements defined in the model.
+   * - digest
+     - string
+     - 0..1
+     - A sha512t24u digest created using the VRS Computed Identifier algorithm.
+   * - expressions
+     - :ref:`Expression`
+     - 0..m
+     - None
+   * - location
+     - :ref:`IRI` | :ref:`SequenceLocation`
+     - 0..1
+     - The location of the Allele
+   * - state
+     - :ref:`LiteralSequenceExpression` | :ref:`ReferenceLengthExpression` | :ref:`LengthExpression`
+     - 0..1
+     - An expression of the sequence state
---
+++ examples/pydantic_to_rst.py
@@ -0,0 +1,108 @@
+from annotated_types import MaxLen, MinLen
+from pydantic import BaseModel
+from pydantic._internal._model_construction import ModelMetaclass
+from typing import Annotated, Any, List
+from typing import get_args, get_origin
+
+from pydantic_to_json_schema import Allele
+
+
+EXCLUDE_PROPS = {"maturity"}
+PYTHON_TO_JSON_TYPES = {
+    str: "string",
+    int: "integer",
+    float: "float",
+    None: "null",
+    list: "array",
+    bool: "boolean",
+    dict: "object",
+}
+
+
+def get_field_type(field_anno: Any) -> str | None:
+    if field_anno in PYTHON_TO_JSON_TYPES:
+        field_type = PYTHON_TO_JSON_TYPES[field_anno]
+    elif isinstance(field_anno, ModelMetaclass):
+        field_type = f":ref:`{field_anno.__name__}`"
+    elif get_origin(field_anno) is Annotated:
+        field_type = "string"
+    else:
+        field_type = None
+    return field_type
+
+
+def get_limits(
+    field_name: str, field_metadata: list[MinLen | MaxLen], field_is_list: bool
+) -> list[str]:
+    min_length = None
+    max_length = None
+
+    if field_metadata:
+        if len(field_metadata) == 2:
+            min_length = field_metadata[0].min_length
+            max_length = field_metadata[1].max_length
+        else:
+            min_length = getattr(field_metadata[0], "min_length", None)
+            max_length = getattr(field_metadata[0], "max_length", None)
+
+    if min_length is not None:
+        limits = [min_length]
+    else:
+        limits = [1 if field_name == "type" else 0]
+
+    if max_length is not None:
+        limits.append(max_length)
+    else:
+        limits.append("m" if field_is_list else 1)
+    return [str(limit) for limit in limits]
+
+
+def generate_rst(model: BaseModel) -> str:
+    lines = [
+        ".. list-table::",
+        "   :class: clean-wrap",
+        "   :header-rows: 1",
+        "   :align: left",
+        "   :widths: auto",
+        "",
+        "   * - Field",
+        "     - Type",
+        "     - Limits",
+        "     - Description",
+    ]
+
+    for field_name, field_info in model.model_fields.items():
+        if field_name in EXCLUDE_PROPS:
+            continue
+
+        field_annotation = tuple(
+            anno for anno in get_args(field_info.annotation) if anno is not type(None)
+        )
+
+        field_is_list = get_origin(field_annotation[0]) in {list, List}
+        limits = get_limits(field_name, field_info.metadata, field_is_list)
+
+        if len(field_annotation) > 1:
+            field_type = " | ".join([get_field_type(anno) for anno in field_annotation])
+        else:
+            field_type = get_field_type(
+                get_args(field_annotation[0])[0]
+                if field_is_list
+                else field_annotation[0]
+            )
+
+        lines.extend(
+            [
+                f"   * - {field_name}",
+                f"     - {field_type}",
+                f"     - {'..'.join(limits)}",
+                f"     - {field_info.description}",
+            ]
+        )
+
+    return "\n".join(lines)
+
+
+if __name__ == "__main__":
+    with open("examples/Allele.rst", "w") as wf:
+        wf.write(generate_rst(Allele))
