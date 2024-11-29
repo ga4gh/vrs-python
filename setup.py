3959e21889a12578e4b3393717dfc373f2424f81
James Stevenson <james.stevenson@nationwidechildrens.org>
2024-11-29 12:28:31 -0500

    add stupid manual print debugging

--- setup.py
+++ setup.py
@@ -17,10 +17,13 @@ def _get_vrs_submodule_info() -> tuple[str, str, int]:
         commit = subprocess.check_output(
             ["git", "rev-parse", "HEAD"], cwd=vrs_path, text=True
         ).strip()
+        print("hey i'm debugging here")
         subprocess.run(["git", "fetch", "--tags"])
+        print("hey i'm debugging here")
         tag = subprocess.check_output(
             ["git", "describe", "--tags", "--abbrev=0"], cwd=vrs_path, text=True
         ).strip()
+        print("hey i'm debugging here")
         distance =  subprocess.check_output(
             ["git", "rev-list", f"{tag}..HEAD", "--count"],
             cwd=vrs_path,
