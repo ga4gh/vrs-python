9d95286234fc1d3d253e94d014b30bd2dd5e2d81
James Stevenson <james.stevenson@nationwidechildrens.org>
2025-01-30 16:54:51 -0500

    pydantic.py

--- src/ga4gh/core/pydantic.py
+++ src/ga4gh/core/pydantic.py
@@ -1,13 +1,15 @@
+"""Provide helpful methods related to pydantic objects."""
 import re
-from typing import Any, Union
+from typing import Any
+
 from pydantic import BaseModel, RootModel
 
 
-def getattr_in(obj, names) -> Any:
-    """
-    Calls getattr on obj and the successive returns from getattr
+def getattr_in(obj, names) -> Any:  # noqa: ANN001
+    """Call getattr on obj and the successive returns from getattr
     using names as attribute names in order. If any return is None,
     terminate early, and return None.
+
     Like clojure (get-in obj names) or (some-> obj .name1 .name2 ...)
     """
     names_i = 0
@@ -22,8 +24,8 @@ def getattr_in(obj, names) -> Any:
 
 
 def is_curie_type(o: Any) -> bool:
-    """
-    Returns true if the object is a str-like matching the CURIE pattern.
+    """Return true if the object is a str-like matching the CURIE pattern.
+
     If object is a Pydantic custom root type, extracts the value first,
     which enables (for example) a GA4GH iriReference pydantic model object to be passed.
     """
@@ -34,26 +36,24 @@ def is_curie_type(o: Any) -> bool:
     return False
 
 
-def is_pydantic_instance(o: Any) -> bool:
+def is_pydantic_instance(o: Any) -> bool:  # noqa: D103
     return isinstance(o, BaseModel)
 
 
-def get_pydantic_root(obj: Union[Any, RootModel]) -> Any:
-    """
-    If o is a Pydantic custom root type, return the root object, else return the input obj
-    """
+def get_pydantic_root(obj: Any | RootModel) -> Any:
+    """If o is a Pydantic custom root type, return the root object, else return the input obj"""
     if isinstance(obj, RootModel):
         return obj.root
     return obj
 
 
-def pydantic_copy(obj: BaseModel) -> BaseModel:
+def pydantic_copy(obj: BaseModel) -> BaseModel:  # noqa: D103
     pydantic_class = type(obj)
     if not issubclass(pydantic_class, BaseModel):
-        raise RuntimeError("Argument was not a pydantic model: " + str(pydantic_class))
+        msg = f"Argument was not a pydantic model: {pydantic_class!r}"
+        raise RuntimeError(msg)  # noqa: TRY004
 
     # Treat RootModel differently, it's a thin wrapper of another object, has no fields
     if issubclass(pydantic_class, RootModel):
         return pydantic_class(obj.model_dump())
-    else:
-        return pydantic_class(**obj.model_dump())
+    return pydantic_class(**obj.model_dump())
