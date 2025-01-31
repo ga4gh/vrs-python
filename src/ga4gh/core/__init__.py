1267f8de29e3ef3efe32ce891704a13dcf7c7b74
James Stevenson <james.stevenson@nationwidechildrens.org>
2025-01-31 08:31:18 -0500

    imports

--- src/ga4gh/core/__init__.py
+++ src/ga4gh/core/__init__.py
@@ -2,9 +2,9 @@
 
 from importlib.metadata import version, PackageNotFoundError
 
-from .digests import sha512t24u
-from .enderef import ga4gh_enref, ga4gh_deref
-from .identifiers import (
+from ga4gh.core.digests import sha512t24u
+from ga4gh.core.enderef import ga4gh_enref, ga4gh_deref
+from ga4gh.core.identifiers import (
     ga4gh_digest,
     ga4gh_identify,
     ga4gh_serialize,
@@ -18,29 +18,29 @@ from .identifiers import (
     GA4GH_DIGEST_REGEXP,
     PrevVrsVersion,
 )
-from .pydantic import is_pydantic_instance, is_curie_type, pydantic_copy
-from . import models as core_models
+from ga4gh.core.pydantic import is_pydantic_instance, is_curie_type, pydantic_copy
+from ga4gh.core import models as core_models
 
 __all__ = [
-    "sha512t24u",
-    "ga4gh_enref",
-    "ga4gh_deref",
-    "ga4gh_digest",
-    "ga4gh_identify",
-    "ga4gh_serialize",
-    "is_ga4gh_identifier",
-    "VrsObjectIdentifierIs",
-    "use_ga4gh_compute_identifier_when",
     "CURIE_NAMESPACE",
     "CURIE_SEP",
-    "GA4GH_PREFIX_SEP",
-    "GA4GH_IR_REGEXP",
     "GA4GH_DIGEST_REGEXP",
+    "GA4GH_IR_REGEXP",
+    "GA4GH_PREFIX_SEP",
     "PrevVrsVersion",
-    "is_pydantic_instance",
-    "is_curie_type",
-    "pydantic_copy",
+    "VrsObjectIdentifierIs",
     "core_models",
+    "ga4gh_deref",
+    "ga4gh_digest",
+    "ga4gh_enref",
+    "ga4gh_identify",
+    "ga4gh_serialize",
+    "is_curie_type",
+    "is_ga4gh_identifier",
+    "is_pydantic_instance",
+    "pydantic_copy",
+    "sha512t24u",
+    "use_ga4gh_compute_identifier_when",
 ]
 
 try:
