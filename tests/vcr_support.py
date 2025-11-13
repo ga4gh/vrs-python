94ad73525587778ca9061da203d9e5c1773f592e
Kyle Ferriter <kferrite@broadinstitute.org>
2025-11-13 12:13:50 -0500

    add decode_compressed_response=True to vcr config

--- tests/vcr_support.py
+++ tests/vcr_support.py
@@ -9,4 +9,5 @@ test_data_dir = Path(test_dir) / "data" / "cassettes"
 vcr = vcrpy.VCR(
     cassette_library_dir=test_data_dir,
     record_mode=os.environ.get("VCR_RECORD_MODE", "new_episodes"),
+    decode_compressed_response=True,
 )
